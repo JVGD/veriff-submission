8117237eb149c951c7d65cea2d9a8ddf29beddbc
Javier Vargas <j.vargas.g.d@gmail.com>
2021-12-13 16:54:23 +0000

    Added docstring

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import yaml
 from src.trainer import train
 
 
+# Entrypoint from the docker container
+# this files calls the trainer with
+# the given conf mounted in /conf
+
+
 if __name__ == "__main__":
     # Reading YAML conf (/conf needs to be mounted in Docker Container)
     with open("/conf/configuration.yaml", "r") as f:
