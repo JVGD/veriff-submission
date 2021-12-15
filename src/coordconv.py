c117fead575f42b8ef40307d65cf1c6de15eb2af
Javier Vargas <j.vargas.g.d@gmail.com>
2021-12-15 10:31:18 +0000

    Added CoordConv2d

--- src/coordconv.py
+++ src/coordconv.py
@@ -42,3 +42,44 @@ class AddCords(nn.Module):
                                       repeat([B,1,H,1])/(W/2)-1)
         # Concatenation and return
         return T.cat([x, i, j], dim=1)
+
+
+class CoordConv(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,
+                 kernel_size: int, *args, **kwargs) -> None:
+        """CoordConv init
+
+        Supports all the parameters from `nn.Conv2d` with
+        argument expansion: `*args` and `**kwargs`
+
+        Args:
+            in_channels (int): Input channels to Conv
+            out_channels (int): Out channels from Conv
+            kernel_size (int): Kernel size of conv operation
+        """
+        # Init base class
+        super().__init__()
+
+        # CoordConv is coordinate channels adder + standar conv
+        # where standar conv has to process 2 extra channels
+        self.add_coord = AddCords()
+        self.conv = nn.Conv2d(in_channels=in_channels+2,
+                              out_channels=out_channels,
+                              kernel_size=kernel_size,
+                              *args, **kwargs)
+
+    def forward(self, x: T.Tensor) -> T.Tensor:
+        """Forward of CoordConv: add coord + conv
+
+        Args:
+            x (T.Tensor): Input image or feature map
+
+        Returns:
+            T.Tensor: Output feature map
+        """
+        # Adding extra coordinate channels
+        x = self.add_coord(x)
+
+        # Standar conv
+        x = self.conv(x)
+        return x
